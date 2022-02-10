99a52552e53c007b8465dcf15f95a65ba8b1d942
Zach Prince <zachary.s.prince@outlook.com>
2022-02-09 22:23:00 -0500

    Complete pair challenge

---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+#Favorite Books
+- Slaughterhouse Five
+- Gravity's Rainbow
+- The Remains of the Day
+- Atonement 
+- The Sun Also Rises
---
+++ challenge/pair_challenge.rb
@@ -0,0 +1,15 @@
+# I chose, "pair_challenge" to annotate that this file was used for the pair challenge in mod 0
+
+
+number_of_books = 5
+# This line will make the variable, "checked_out" equal the boolean true
+checked_out = true
+# This line will make the variable, "library_rule" equal the string, "No talking"
+library_rule = "No talking."
+# This line will make the variable, "library_open" equal the boolean true
+library_open = false
+# This line will make the variable, "library_cards" equal the integer 78
+library_cards = 78
+book_title = "To Catch a Mockingbird"
+
+popular_books = ["Slaughterhouse Five", "Gravity's Rainbow", "The Remians of the Day", "Atonement", "The Sun Also Rises"]
